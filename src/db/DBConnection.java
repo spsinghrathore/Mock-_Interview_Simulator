36f400459107be777a3f8e17e862c5d93925607b
SP <124254399+spsinghrathore@users.noreply.github.com>
2025-06-27 17:25:55 +0530

    Update DBConnection.java

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String url = "jdbc:mysql://localhost:3306/interview_master";
     private static final String username = "root";
-    private static final String password = "Admin123";
+    private static final String password = "Your Password";
 
     private static Connection connection;
 
